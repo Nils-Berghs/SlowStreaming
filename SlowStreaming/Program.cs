915e9fc0937e2c13b4f282826f9539080eb09d02
Nils Berghs <Nils.Berghs@inetum-realdolmen.world>
2025-04-08 08:20:12 +0200

    Add project files.

---
+++ SlowStreaming/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SlowStreaming;
+using System.Diagnostics;
+using System.Reflection.Metadata;
+
+Console.WriteLine("Hello, World!");
+
+SlowStream stream = new SlowStream("C:\\Users\\NBGCT87\\Downloads\\BingWallpaper.exe");
+
+using HttpClient client = new HttpClient();
+
+//using var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:7250/Requests");
+using var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:7250/Requests/Stream");
+
+var streamContent = new StreamContent(stream);
+streamContent.Headers.ContentType = new("application/octet-stream");
+
+var stringContent = new StringContent("Hier");
+stringContent.Headers.ContentType = new("application/json");
+
+var content = new MultipartFormDataContent
+{
+      {new StreamContent(stream), "data", "fileName.exe" },
+      {new StringContent("Hier"), "Municipality"},
+};
+request.Content = content;
+Console.WriteLine("Starting send");
+var result = await client.SendAsync(request);
+
+Debug.WriteLine("done, press any key to exit");
+Console.Read();
---
+++ SlowStreaming/SlowStream.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlowStreaming
+{
+    internal class SlowStream : Stream
+    {
+        private FileStream FileStream { get; }
+
+        public override bool CanRead => FileStream.CanRead;
+
+        public override bool CanSeek => FileStream.CanSeek;
+
+        public override bool CanWrite => FileStream.CanWrite;
+
+        public override long Length => FileStream.Length;
+
+        public override long Position 
+        { 
+            get => FileStream.Position; 
+            set => FileStream.Position = value; 
+        }
+
+        public SlowStream(string fileName)
+        {
+            FileStream = new FileStream(fileName, FileMode.Open);
+        }
+
+        public override void Flush()
+        {
+            FileStream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            Console.WriteLine($"reading {count} bytes at offset {offset}, position = {Position}, length = {Length}");
+            Thread.Sleep(100);
+            return FileStream.Read(buffer, offset, count);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return FileStream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            FileStream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            FileStream.Write(buffer, offset, count);
+        }
+    }
+}
